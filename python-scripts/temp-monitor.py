919d161e2ea65655e6355908484b5a689a986135
mtarsel <mtarsel@gmail.com>
2014-10-14 01:30:49 -0400

    working code to read from serial port and get temperature

--- python-scripts/temp-monitor.py
+++ python-scripts/temp-monitor.py
@@ -1,44 +1,33 @@
+#
+#   Author:
+#       Mick Tarsel
+#
+# The code below was tested with the Arduino UNO board and TMP36 Temperature Sensor
+#
+#   This will read from the serial port with an arduino and parse the import.
+#
+#
+#
+#
+
+
+
 import serial
 #pip install pyserial
 
-def receiving(ser):
-    print "inside receiving..."
-    global last_received
+arduino_serial_port='/dev/ttyACM0'
 
-    buffer = ''
-    while True:
-        buffer = buffer + ser.read(ser.inWaiting())
-        if '\n' in buffer:
-            lines = buffer.split('\n') # Guaranteed to have at least 2 entries
-            last_received = lines[-2]
-            #If the Arduino sends lots of empty lines, you'll lose the
-            #last filled line, so you could make the above statement conditional
-            #like so: if lines[-2]: last_received = lines[-2]
-            buffer = lines[-1]
-
-arduino_serial_port="/dev/ttyACM0"
-
-#ser.open()
-#ser.isOpen()
 print "Opening serial port..."
-ser = serial.Serial(arduino_serial_port,24000,timeout=0)
-#ser = serial.Serial(arduino_serial_port,2400,timeout=10)
-
+ser = serial.Serial(arduino_serial_port,9600)
 
 print "Reading from serial port..."
-#print ser.read()
+#print ser.readline()
 
-receiving(ser)
+#read data forever
+while True:
 
-
-#print "Entire sensor: ", sensor
-
-
-#print "sensor[0]: ", sensor[0]
-
-#print "sensor[1]: ", sensor[1]
-
-# temperature = (((temperature - .5)*100)*1.8)+32;  //this line is for Fahrenheit                                             
+    #each temperature is split by a '\n' so lets use split() and parse it
+    print ser.readline().split()
 
 ser.close()
 print "Closed the serial port. Exiting."
