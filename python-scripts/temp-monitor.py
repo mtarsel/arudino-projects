9c79aca1ba8ef04122fa39b9312596496065eaec
mtarsel <mtarsel@gmail.com>
2014-10-14 02:13:54 -0400

    simple print out with timestamp and temp

--- python-scripts/temp-monitor.py
+++ python-scripts/temp-monitor.py
@@ -6,16 +6,19 @@
 #
 #   This will read from the serial port with an arduino and parse the import.
 #
-#
-#
-#
 
 
-
+import csv
 import serial
 #pip install pyserial
+import datetime
+import time
 
 arduino_serial_port='/dev/ttyACM0'
+csv_file = 'temperature.csv'
+
+
+timestamp_temp={}
 
 print "Opening serial port..."
 ser = serial.Serial(arduino_serial_port,9600)
@@ -26,8 +29,18 @@ print "Reading from serial port..."
 #read data forever
 while True:
 
-    #each temperature is split by a '\n' so lets use split() and parse it
-    print ser.readline().split()
+
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+        #each temperature is split by a '\n' so lets use split() and parse it
+
+        print "timestamp, temperature: ", st, " ", ser.readline().split()
+
+
+        #store it in a dictionary for anarchy
+       # timestamp_temp[st] = ser.readline().split()
+
+        #print timestamp_temp
 
 ser.close()
 print "Closed the serial port. Exiting."
