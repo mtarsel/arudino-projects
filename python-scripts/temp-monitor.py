abf9db55a914c7d1ee3e868f98bfb20977eff501
mtarsel <mtarsel@gmail.com>
2014-10-14 00:22:52 -0400

    Does not read from serial port

--- python-scripts/temp-monitor.py
+++ python-scripts/temp-monitor.py
@@ -3,12 +3,14 @@ import serial
 import os
 import time
 import sys
-#import MySQLdb #not needed at this time
 
 arduino_serial_port = "/dev/ttyACM0"
 print "Opened serial port..."
+
 ser = serial.Serial(arduino_serial_port,24000,timeout=0)
 #ser = serial.Serial(arduino_serial_port,2400,timeout=10)
+
+
 print "Reading from serial port..."
 ser.readline()
 
@@ -21,27 +23,6 @@ print "sensor[0]: ", sensor[0]
 
 print "sensor[1]: ", sensor[1]
 
-#def getData():
-#        sensor = ser.readline().split()
-#        print sensor[0]
-#        print sensor[1]
-#        conn = MySQLdb.connect (host = "localhost", user = "user", passwd = "password", db = "db_name")
-#        cursor = conn.cursor ()
-
- #       command = "INSERT INTO `monitor`.`data` (`id`, `sensor_id`, `time`, `temp`) VALUES (NULL, '%s', CURRENT_TIMESTAMP, '%s');" % (sensor[0], sensor[1])
- #       cursor.execute(command);
- #       cursor.close ()
-  #      conn.close ()
-#        return
-
-
-
-#for i in range(5):
-#        getData()
 
 ser.close()
-print "Closed the serial port"
-
-
-#getData for all sensors
-#writeToDataBase(sensor, temp)
+print "Closed the serial port. Exiting."
