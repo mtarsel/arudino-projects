c230f60ccc7572e1b73e30b4144c58b4e1abbdcf
mtarsel <mtarsel@gmail.com>
2014-10-26 22:05:25 -0400

    Added button code with TIP120

---
+++ button-transistor/button_transistor/button_transistor.ino
@@ -0,0 +1,55 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+  The circuit:
+ * LED connected from digital pin 13 to ground.
+  * Note: On most Arduino boards, there is already an LED on the board
+ connected to pin 13, so you don't need any extra components for this example.
+  
+ Created 1 June 2005
+ By David Cuartielles
+ http://arduino.cc/en/Tutorial/Blink
+ based on an orginal by H. Barragan for the Wiring i/o board
+ */
+
+int lockPin =  9;    // Relay connected to digital pin 2   <-----Change this to pin 2
+int buttonPin =  2;    // Relay connected to digital pin 2   <-----Change this to pin 2
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+// The setup() method runs once, when the sketch starts
+
+void setup()   {                
+  // initialize the digital pin as an output:
+  pinMode(lockPin, OUTPUT);   
+    pinMode(buttonPin, OUTPUT);    
+}
+
+// the loop() method runs over and over again,
+// as long as the Arduino has power
+
+void loop()                     
+{
+  
+  buttonState = digitalRead(buttonPin);
+  
+  
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {     
+    // turn LED on:    
+     digitalWrite(lockPin, HIGH);   // set the LED on
+     delay(2000);                  // wait for a second
+         digitalWrite(lockPin, LOW); 
+
+  } else {
+    // turn LED off:
+    digitalWrite(lockPin, LOW); 
+  }
+  
+ // digitalWrite(lockPin, HIGH);   // set the LED on
+ // delay(2000);                  // wait for a second
+ // digitalWrite(lockPin, LOW);    // set the LED off
+ // delay(5000);                  // wait for a second
+}
